e2303177a3ed8cf89d7dbeb712bd0b61e9d21497
unknown <albertvalvila@gmail.com>
2021-01-28 16:17:53 +0100

    Added Unreal Files & updated gitignore with Rider folders

---
+++ Source/UnrealGameJam/UnrealGameJam.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealGameJam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealGameJam, "UnrealGameJam" );
